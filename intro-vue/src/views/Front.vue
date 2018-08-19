69d402dbaa11e5625ec18079a979c38c62f01b28
Natalie <natalie.todd15@gmail.com>
2018-08-19 15:44:32 -0600

    got rid of footer on front page

--- intro-vue/src/views/Front.vue
+++ intro-vue/src/views/Front.vue
@@ -2,19 +2,16 @@
 <div id='first'>
     <router-view></router-view>
         <Modal />
-    <Footer />
     </div>
     
 </template>
 
 <script>
 import Modal from '@/components/Modal';
-import Footer from '@/components/Footer';
 export default {
   name: 'first',
   components: {
-    Modal,
-    Footer,
+    Modal
   },
 };
 </script>
