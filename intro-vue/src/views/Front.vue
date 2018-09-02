d0c2bf5ed4183e1ebd22b6721868f2b59fc5fe77
Natalie <natalie.todd15@gmail.com>
2018-09-02 13:18:50 -0600

    added responsiveness to background image

--- intro-vue/src/views/Front.vue
+++ intro-vue/src/views/Front.vue
@@ -11,22 +11,29 @@ import Modal from "@/components/Modal";
 export default {
   name: "first",
   components: {
-    Header, 
+    Header,
     Modal
   }
 };
 
-document.body.style.backgroundImage = "url('https://www.maxpixel.net/static/photo/2x/Nature-Pakistan-Travel-Landscape-Forest-Tree-Lake-2366663.jpg')";
+document.body.style.backgroundImage =
+  "url('https://www.maxpixel.net/static/photo/2x/Nature-Pakistan-Travel-Landscape-Forest-Tree-Lake-2366663.jpg')";
 </script>
 
 <style>
-.first {
+/* .first {
   height: 100vh;
-  /* background-image: url('./assets/green-hil.jpg'); */
-}
+} */
 body {
   height: 100vh;
-    background-size: 100% auto;
-    background-repeat: no-repeat;
+  background-size: 100% auto;
+  background-repeat: no-repeat;
+  background: fluid;
+  /* width: 100%; */
+}
+@media (max-width: 767px) {
+  body {
+    background: fixed;
+  }
 }
 </style>
