1cb7be3116ff3689c205ba5c64c092eb5698a2b3
Natalie <natalie.todd15@gmail.com>
2018-09-02 12:39:56 -0600

    added background image

--- intro-vue/src/views/Front.vue
+++ intro-vue/src/views/Front.vue
@@ -3,7 +3,6 @@
     <router-view></router-view>
     <Header />
         <Modal />
-            <!-- <Hero /> -->
     </div>
 </template>
 <script>
@@ -16,10 +15,18 @@ export default {
     Modal
   }
 };
+
+document.body.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2017/06/07/15/53/new-zealand-2380705_1280.jpg')";
 </script>
 
 <style>
 .first {
   height: 100vh;
+  /* background-image: url('./assets/green-hil.jpg'); */
+}
+body {
+  height: 100vh;
+    background-size: 100% auto;
+    background-repeat: no-repeat;
 }
 </style>
